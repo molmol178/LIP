945dd4e64558d45f515cccb39de256002c276981
molmol178 <kazuto.molmol@gmail.com>
2017-03-04 16:51:58 +0900

    add header hiding

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
-function hello(){
-    var msg = "hello world!!!";
-    console.log(msg);
-}
-hello();
-
-function modal(){
-    document.querySelector('dialog').showModal();
-}
+$(document).ready(function(){
+    var menuHeight = $("#target").height();
+    var startPos = 0;
+    $(window).scroll(function(){
+    var currentPos = $(this).scrollTop();
+    if (currentPos > startPos) {
+        if($(window).scrollTop() >= 200) {
+        $("#target").css("top", "-" + menuHeight + "px");
+        }
+    } else {
+        $("#target").css("top", 0 + "px");
+    }
+    startPos = currentPos;
+    });
+});
